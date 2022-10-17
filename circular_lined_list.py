3c5508d0cf23e093d023ff697476b04a0efcd578
Rakesh Gain <rakeshgain47681@gmail.com>
2022-10-17 06:32:41 +0530

    insert at certail position

--- circular_lined_list.py
+++ circular_lined_list.py
@@ -22,6 +22,25 @@ class SingleList:
              self.tail.next = self.head   
         return
 
+    def insert(self,index,data):
+        new_node = Node(data)
+        if index<1:
+            print("Invalid position")
+        elif index == 1:
+            current = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = new_node
+            new_node.next = self.head
+            self.head = new_node
+        else:        
+            current = self.head
+            for i in range(1,index-1):
+                current = current.next
+            new_node.next = current.next   
+            current.next = new_node 
+        return    
+          
     def displayList(self):
         current = self.head
         if self.head == None:
@@ -38,4 +57,10 @@ if __name__ == "__main__":
     llist = SingleList()
     llist.Append(10)
     llist.Append(20)
+    llist.Append(30)
+    llist.displayList()
+    # llist.insert(3,50)
+    # llist.insert(4,23)
+    llist.insert(1,8)
+    print()
     llist.displayList()
