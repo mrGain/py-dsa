8793d74ed84eb106b87a581a54694247ddd4e617
Rakesh Gain <rakeshgain47681@gmail.com>
2022-11-16 20:33:37 +0530

    basic tree in python

---
+++ tree/create_tree.py
@@ -0,0 +1,22 @@
+class TreeNode:
+    def __init__(self, data, children):
+        self.data = data 
+        self.children = children
+
+    def __str__(self, level = 0):
+        ret = " " * level + self.data + '\n'
+
+        for child in self.children:
+            ret += child.__str__(level + 1)
+        return ret 
+
+    def addChild(self, TreeNode):
+        self.children.append(TreeNode)
+
+tree = TreeNode('Drinks', [])
+cold = TreeNode('cold', [])
+hot = TreeNode('hot',[])
+tree.addChild(cold)
+tree.addChild(hot)
+print(tree) 
+ 
