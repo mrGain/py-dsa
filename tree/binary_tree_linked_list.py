17427a3e1d24577adc82b98658b6653b7cdf263e
Rakesh Gain <rakeshgain47681@gmail.com>
2022-11-19 14:38:00 +0530

    pre-order traversal

--- tree/binary_tree_linked_list.py
+++ tree/binary_tree_linked_list.py
@@ -2,6 +2,21 @@ class TreeNode:
     def __init__(self,data):
         self.leftChild = None 
         self.data = data 
-        self.rightchild = None 
+        self.rightChild = None 
 
-newBT = TreeNode('Dtinks')        
+newBT = TreeNode('Dtinks')
+leftChild = TreeNode('Hot')        
+rightChild = TreeNode('Cold')
+newBT.leftChild = leftChild
+newBT.rightChild = rightChild
+
+# Pre-order Traversal ( root-->left-->right )
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return 
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)    
+    preOrderTraversal(rootNode.rightChild)
+
+preOrderTraversal(newBT)        
+
